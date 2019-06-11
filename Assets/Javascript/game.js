466b0e71089d3a3d7c294c1187451f911fcce320
Sean Esqueda <sesqueda99@aol.com>
2019-06-11 16:45:48 -0700

    Finished up css and html while adding some javascript

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,8 +1,40 @@
 $( document ).ready(function() {
 
-    //Create a variable that gets a random number between 19-120
-    randomNumber = Math.floor(Math.random() * (121 - 19) ) + 19;
+    //Global variable for counting and crystal values
+    var counter = 0;
+    var crystal1 = 0;
+    var crystal2 = 0;
+    var crystal3 = 0;
+    var crystal4 = 0;
+
+    //Create a function that gets a random number between 19-120
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+    //Function that distributes variables on load
+    randomNumber = getRndInteger(19,120);
+    crystal1 = getRndInteger(1,12);
+    crystal2 = getRndInteger(1,12);
+    crystal3 = getRndInteger(1,12);
+    crystal4 = getRndInteger(1,12);
     //Display to screen
     $('#randomNumber').text(randomNumber);
-
+    
+    //Create functions for clicking on crystals
+    $("#crystal1").on("click",function() {
+        counter = counter + crystal1;
+        $("#totalScore").text(counter);
+    });
+    $("#crystal2").on("click",function() {
+        counter = counter + crystal2;
+        $("#totalScore").text(counter);
+    });
+    $("#crystal3").on("click",function() {
+        counter = counter + crystal3;
+        $("#totalScore").text(counter);
+    });
+    $("#crystal4").on("click",function() {
+        counter = counter + crystal4;
+        $("#totalScore").text(counter);
+    });
 });
