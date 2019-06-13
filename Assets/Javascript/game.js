db0237f95aca9e9734935e66a5b943c4d1791aca
Sean Esqueda <sesqueda99@aol.com>
2019-06-12 22:12:21 -0700

    Finish javascript & adjust html elements

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,40 +1,69 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
     //Global variable for counting and crystal values
     var counter = 0;
+    var wins = 0;
+    var losses = 0;
     var crystal1 = 0;
     var crystal2 = 0;
     var crystal3 = 0;
     var crystal4 = 0;
 
-    //Create a function that gets a random number between 19-120
+    //Create a function that gets a random number
     function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    //Function that distributes variables on load
-    randomNumber = getRndInteger(19,120);
-    crystal1 = getRndInteger(1,12);
-    crystal2 = getRndInteger(1,12);
-    crystal3 = getRndInteger(1,12);
-    crystal4 = getRndInteger(1,12);
-    //Display to screen
-    $('#randomNumber').text(randomNumber);
-    
+
+    //Function to start the game
+    function startGame() {
+        //Generate random number
+        randomNumber = getRndInteger(19, 120);
+        //Generate random value for each crystal
+        crystal1 = getRndInteger(1, 12);
+        crystal2 = getRndInteger(1, 12);
+        crystal3 = getRndInteger(1, 12);
+        crystal4 = getRndInteger(1, 12);
+        //Display to screen
+        $('#randomNumber').text(randomNumber);
+        counter = 0;
+        $("#totalScore").text(counter);
+    }
+
+    //Create function that checks if we win or we lose
+    function checkWin() {
+        if (counter === randomNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            startGame();
+        }
+        else if (counter > randomNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses);
+            startGame();
+        }
+    }
     //Create functions for clicking on crystals
-    $("#crystal1").on("click",function() {
+    $("#crystal1").on("click", function () {
         counter = counter + crystal1;
         $("#totalScore").text(counter);
+        checkWin();
     });
-    $("#crystal2").on("click",function() {
+    $("#crystal2").on("click", function () {
         counter = counter + crystal2;
         $("#totalScore").text(counter);
+        checkWin();
     });
-    $("#crystal3").on("click",function() {
+    $("#crystal3").on("click", function () {
         counter = counter + crystal3;
         $("#totalScore").text(counter);
+        checkWin();
     });
-    $("#crystal4").on("click",function() {
+    $("#crystal4").on("click", function () {
         counter = counter + crystal4;
         $("#totalScore").text(counter);
+        checkWin();
     });
+
+    //Start the game on load
+    startGame();
 });
